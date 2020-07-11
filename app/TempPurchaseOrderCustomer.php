1e592bc9a6f4aab589ac984e046757f9dc068942
fachrz <rfachru3@outlook.com>
2020-07-11 10:03:31 +0700

    add detail purchase order

---
+++ app/TempPurchaseOrderCustomer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TempPurchaseOrderCustomer extends Model
+{
+    protected $table = 'temp_purchase_order_customers';
+    protected $primaryKey = 'temp_po_id';
+    protected $fillable = ['temp_po_id', 'po_id', 'po_num', 'id_penawaran', 'customer_id', 'po_note', 'po_discount', 'po_discount_type', 'id_user','expr'];
+    public $timestamps = false;
+}
---
+++ database/seeds/PurchaseOrderCustomerSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PurchaseOrderCustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('purchase_order_customers')->insert([
+            'po_id' => '023984/02934',
+            'po_num' => '23984791/2349',
+            'id_penawaran' => '543234sfdg',
+            'customer_id' => 1,
+            'po_note' => 'Lorem Ipsum Dolor sit amet',
+            'po_discount' => 200,
+            'po_discount_type' => '%',
+            'id_user' => 1
+        ]);
+    }
+}
